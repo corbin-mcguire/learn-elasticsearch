288223b56bbf1f5b578b0b6fb1e800b2e9d08311
Corbin McGuire <mcguire.corbin@gmail.com>
2024-07-01 23:53:16 -0400

    Added docker network creation to start script

--- bin/start_es.sh
+++ bin/start_es.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+exists=$(docker network ls | grep elk-network)
+
+if [[ -z $exists ]]
+ then
+  echo "Creating network for ELK stack"
+  docker network create elk-network
+fi
+
 docker compose -f docker-compose.es.yml up -d
